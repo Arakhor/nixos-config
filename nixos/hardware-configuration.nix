6063ec74ba860949ef7970925d9305c13cfe282f
arakhor <csmolenski23@gmail.com>
2023-12-27 12:18:22 +0100

    fix

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -14,29 +14,8 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "tmpfs";
-      fsType = "tmpfs";
-    };
-
-  fileSystems."/iso" =
-    { device = "/dev/disk/by-uuid/1980-01-01-00-00-00-00";
-      fsType = "iso9660";
-    };
-
-  fileSystems."/nix/.ro-store" =
-    { device = "/iso/nix-store.squashfs";
-      fsType = "squashfs";
-      options = [ "loop" ];
-    };
-
-  fileSystems."/nix/.rw-store" =
-    { device = "tmpfs";
-      fsType = "tmpfs";
-    };
-
-  fileSystems."/nix/store" =
-    { device = "overlay";
-      fsType = "overlay";
+    { device = "/dev/disk/by-uuid/cff14c9d-99c7-409e-9954-520327a7825e";
+      fsType = "ext4";
     };
 
   swapDevices = [ ];
